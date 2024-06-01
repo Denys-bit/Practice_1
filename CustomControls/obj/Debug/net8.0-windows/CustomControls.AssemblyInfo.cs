ef57b055813c66e3e928eb74a0b1ec74bc1f1ee9
Denys-bit <denisborisenko479@gmail.com>
2024-06-01 21:44:49 +0300

    The second project has been created WFCL

--- CustomControls/obj/Debug/net8.0-windows/CustomControls.AssemblyInfo.cs
+++ CustomControls/obj/Debug/net8.0-windows/CustomControls.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ControlDemoApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CustomControls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fbaa21427a22278bac5bbb126115788e34bc4c")]
-[assembly: System.Reflection.AssemblyProductAttribute("ControlDemoApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ControlDemoApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("CustomControls")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CustomControls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
